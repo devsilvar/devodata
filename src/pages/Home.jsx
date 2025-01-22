e89aa557ec828bf6502aabef7d95f94c7d388378
refsilva <refsilva@yahoo.com>
2025-01-22 06:18:06 +0100

    corrected imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import HeroSection from '../component/Herosection';
+import HeroSection from '../component/HeroSection';
 import DetailsSection from '../component/DetailsSection';
 import AboutCourse from '../component/AboutCourse';
 import Courses from '../component/Courses';
 import Program from '../component/Program';
 import FAQComponent from '../component/FAQComponent';
 
-const Home = () => {
-  return (
-    <main>
-      <HeroSection />
-      <DetailsSection />
-      <AboutCourse />
-      <Courses />
-      <Program />
-      <FAQComponent />
-    </main>
-  );
-};
+const Home = () => (
+  <main>
+    <HeroSection />
+    <DetailsSection />
+    <AboutCourse />
+    <Courses />
+    <Program />
+    <FAQComponent />
+  </main>
+);
 
 export default Home;
