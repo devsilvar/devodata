9339cdc6f10b0d5ee5e3188694362d9c5817a18d
refsilva <refsilva@yahoo.com>
2025-01-22 05:41:00 +0100

    corrected uncessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Navbar from './component/Navbar';
-import HeroSection from './component/Herosection';
-import DetailsSection from './component/DetailsSection';
-import AboutCourse from './component/AboutCourse';
-import Program from './component/Program';
-import FAQComponent from './component/FAQComponent';
 import Footer from './component/Footer';
-import Courses from './component/Courses';
-import CourseDetails from './pages/CourseDetailsPage';
 import { Route, Routes } from 'react-router-dom';
 import Instructors from './pages/Instructors';
 import Contact from './pages/Contact';
